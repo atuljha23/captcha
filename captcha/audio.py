70986a1b910bfb7c6aea84df83b115fa41ac918a
Atul Jha <atul23.jha@gmail.com>
2019-10-07 22:11:24 +0100

    Update audio.py

--- captcha/audio.py
+++ captcha/audio.py
@@ -225,26 +225,6 @@ class AudioCaptcha(object):
         voice = change_sound(voice, level)
         return voice
 
-    def create_wave_body(self, chars):
-        voices = []
-        inters = []
-        for key in chars:
-            voices.append(self._twist_pick(key))
-            v = random.randint(WAVE_SAMPLE_RATE, WAVE_SAMPLE_RATE * 3)
-            inters.append(v)
-
-        durations = map(lambda a: len(a), voices)
-        length = max(durations) * len(chars) + reduce(operator.add, inters)
-        bg = self.create_background_noise(length, chars)
-
-        # begin
-        pos = inters[0]
-        for i, v in enumerate(voices):
-            end = pos + len(v) + 1
-            bg[pos:end] = mix_wave(v, bg[pos:end])
-            pos = end + inters[i]
-
-        return BEEP + SILENCE + BEEP + SILENCE + BEEP + bg + END_BEEP
 
     def generate(self, chars):
         """Generate audio CAPTCHA data. The return data is a bytearray.
